424d65230fa8e7bd08547752f5ed3093863adc82
Demon <Demonid3e@gmail.com>
2021-01-07 13:47:16 +0000

    Second homework v1

--- script.js
+++ script.js
@@ -10,12 +10,30 @@ const personalMovieDB = {
   private: false,
 };
 
-const a = prompt("Last watched film?", ""),
-  b = +prompt("How will you rate it?", ""),
-  c = prompt("Last watched film?", ""),
-  d = +prompt("How will you rate it?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+for (let i = 0; i < 2; i++) {
+  const film = prompt("Last watched film?", ""),
+    rating = +prompt("How will you rate it?", "");
+  if (
+    film != null &&
+    rating != null &&
+    film != "" &&
+    rating != "" &&
+    film.length < 50
+  ) {
+    personalMovieDB.movies[film] = rating;
+    console.log("Done");
+  } else {
+    i--;
+    console.log("Error");
+  }
+}
+if (personalMovieDB.count < 10) {
+  alert("Watched to few movies!");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  alert("You are typical movie lover!");
+} else if (personalMovieDB.count > 30) {
+  alert("You are movie fan!!!");
+} else {
+  alert("Error");
+}
 console.log(personalMovieDB);
