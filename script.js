09de4f513a18dcbd39bea3b20be8ef96f78d0f84
Demon <Demonid3e@gmail.com>
2021-01-08 13:30:29 +0000

    added explanation of function types

--- script.js
+++ script.js
@@ -67,9 +67,29 @@ showFirstText("Hello world");
 // example of scope and global variable
 
 let num = 20;
+// Type of: function declaration, can be called before it is read.
 
 function callNumber() {
   let num = 30;
   console.log(num);
 }
 callNumber();
+
+// Type of: function expression, only called when its read.
+
+const calculus = function (a, b) {
+  return a + b;
+};
+console.log(calculus(5, 3));
+
+// Type of: arrow function, doesnt have contects "THIS???"
+
+const calCulus = (a, b) => {
+  return a + b;
+};
+//or if its only 1 line can write like this
+const balCulus = (a, b) => a + b;
+//or if only 1 argument
+const galCulus = (a) => a + 7;
+
+console.log(galCulus(4));
