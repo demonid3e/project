9b41e13915a7be687a48552000897ab112328d3a
Demon <Demonid3e@gmail.com>
2021-01-18 13:22:03 +0000

    added new examples of dome methods, added test file, updated course info

---
+++ elements/js/script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const box = document.getElementById("box"),
+  btns = document.getElementsByTagName("button"),
+  circles = document.getElementsByClassName("circle"),
+  hearts = document.querySelectorAll(".heart"),
+  oneHeart = document.querySelector(".heart");
+
+// box.style.backgroundColor = "blue";
+// box.style.width = "500px";
+
+// passing cssstyle inline as text
+let num = "500px";
+box.style.cssText = `background-color: green; width: ${num} `;
+
+btns[1].style.borderRadius = "100%";
+circles[0].style.backgroundColor = "green";
+//cant circles.style.backgroundColor = "green";  because "circles" Pseudo object"
+
+// old style method
+for (let i = 0; i < hearts.length; i++) {
+  hearts[i].style.backgroundColor = "blue";
+}
+
+// colors each heart blue in loop
+hearts.forEach((item) => {
+  item.style.backgroundColor = "blue";
+});
+
+// creates a new element "div in this case" it exists only in JS
+const div = document.createElement("div");
+// styled it with .black from css
+div.classList.add("black");
+//now added created element to end of <body>
+//  document.body.append(div);
+
+// creates just text
+const text = document.createTextNode("This is text");
+
+const wrapper = document.querySelector(".wrapper");
+
+// puts element to the beginning
+wrapper.append(div);
+// wrapper.appendChild(div);
+
+// old style
+// wrapper.insertBefore(div, hearts[0]);
+// wrapper.removeChild(hearts[1]);
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+// hearts[0].after(div);
+
+// circles[0].remove();
+
+// hearts[0].replaceWith(circles[0]);
+
+// inserting HTML text into created object"div"
+div.innerHTML = "<h1>Hello World</h1>";
+
+// inserting ONlY text
+// div.textContent = "Hello";
+
+div.insertAdjacentHTML("beforeend", "<h2>Hello</h2>");
