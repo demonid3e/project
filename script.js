c1c55fed4bf88ac6f4961af9acfd12149b002096
Demon <Demonid3e@gmail.com>
2021-01-28 13:52:32 +0000

    Added mobile functions and explanation about async loading

--- script.js
+++ script.js
@@ -1,90 +1,26 @@
 "use strict";
+// DEFER
+// defer tell browser to load script in background
+//and continue to load DOM element then run script.
+// it never blocks loading
+// it will wait until all content is loaded
 
-const personalMovieDB = {
-  count: 0,
-  movies: {},
-  actors: {},
-  genres: [],
-  private: false,
-  start: function () {
-    personalMovieDB.count = +prompt("How many films you watched?", "");
+//ASYNC
+// will not wait until anything is loaded
+// will load as soon as it ready
 
-    while (
-      personalMovieDB.count == "" ||
-      personalMovieDB.count == null ||
-      isNaN(personalMovieDB.count)
-    ) {
-      personalMovieDB.count = +prompt("How many films you watched?", "");
-    }
-  },
-  rememberMyfilms: function () {
-    for (let i = 0; i < 2; i++) {
-      getFilm();
-      getRating();
+//Dynamicly created script will load as async
+//you can add property async false to prevent it
 
-      if (
-        film != null &&
-        rating != null &&
-        film != "" &&
-        rating != "" &&
-        film.length < 50
-      ) {
-        personalMovieDB.movies[film] = rating;
-        console.log("Done");
-      } else {
-        i--;
-        console.log("Error");
-      }
-    }
-  },
-  detectPersonalLevel: function () {
-    if (personalMovieDB.count <= 10) {
-      alert("Watched to few movies!");
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-      alert("You are typical movie lover!");
-    } else if (personalMovieDB.count > 30) {
-      alert("You are movie fan!!!");
-    } else {
-      alert("Error");
-    }
-  },
-  writeYourGenres: function () {
-    for (let i = 1; i <= 3; i++) {
-      personalMovieDB.genres[i - 1] = prompt(`Your favorite genre? ${i}`, "");
-      while (personalMovieDB.genres == "" || personalMovieDB.genres == null) {
-        personalMovieDB.genres[i - 1] = prompt(`Your favorite genre? ${i}`, "");
-      }
-    }
-    personalMovieDB.genres.forEach((item, i) => {
-      console.log(`Favorite genre ${i + 1} is  ${item}!`);
-    });
-  },
-  toggleVisibleMyDB: function () {
-    if (personalMovieDB.private) {
-      personalMovieDB.private = false;
-    } else if (personalMovieDB.private === false) {
-      personalMovieDB.private = true;
-    } else {
-      console.log("Error");
-    }
-  },
-};
-let film;
-let rating;
+const p = document.querySelectorAll("p");
+console.log(p);
 
-const getFilm = function () {
-  film = prompt("Last watched film?", "");
-  return film;
-};
-
-const getRating = function () {
-  rating = +prompt("How will you rate it?", "");
-  return rating;
-};
-
-// checks the argument for true or false
-function showMyDB(hidden) {
-  if (!hidden) console.log(personalMovieDB);
+function loadScript(src) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.async = false;
+  document.body.append(script);
 }
 
-showMyDB(personalMovieDB.private);
+loadScript("test.js");
+loadScript();
