f7c4b3017968c39eba9aaccc82ba0116d7d95e9f
Demon <Demonid3e@gmail.com>
2021-02-11 13:44:28 +0000

    latest course

--- food/js/script.js
+++ food/js/script.js
@@ -1,4 +1,7 @@
 "use strict";
+
+// TABS
+
 document.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
@@ -35,4 +38,107 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     console.dir(event.target);
   });
+
+  // Tmer
+  const deadline = "2021-03-07";
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+    // calling function to prevent default numbers appear when refreshing page
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+    }
+  }
+
+  setClock(".timer", deadline);
+
+  // Modal
+
+  const modalButton = document.querySelectorAll("[data-modal]"),
+    modalClose = document.querySelector("[data-close]"),
+    modal = document.querySelector(".modal");
+
+  function closeModal() {
+    modal.classList.toggle("show");
+    // modal.classList.add("show");
+    // modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  function openModal() {
+    modal.classList.toggle("show");
+    // modal.classList.add("hide");
+    // modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+  modalButton.forEach((item) => {
+    item.addEventListener("click", closeModal);
+  });
+
+  modalClose.addEventListener("click", openModal);
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
 });
--- script.js
+++ script.js
@@ -1,1 +1,84 @@
+"use strict";
 
+function showThis() {
+  console.log(this);
+}
+
+showThis();
+
+function showThat(a, b) {
+  console.log(this);
+  function sum() {
+    console.log(this);
+    return a + b;
+  }
+  console.log(sum());
+}
+
+showThat(4, 8);
+1) Common function: this = window, but if "use strict"  = undefined
+
+
+const obj = {
+  a: 20,
+  b: 15,
+  sum: function () {
+    console.log(this);
+    function shout (){
+        console.log(this);
+    }
+  },
+};
+obj.sum();
+
+2) Object`s method`s context will be object itself.
+(if inside this object will be function which will use "this"
+it will be used in context of window or if "use strict is not used" undefined).
+
+3) "this" in Constructors and Classes - new property of object
+function User (name,id){
+    this.name = name;
+    this.id = id;
+    this.human = true;
+
+}
+
+let ivan = new User("Ivan", 23);
+
+4) Manual change focus of "this" : call, apply, bind
+
+function sayName(age) {
+  console.log(this);
+  console.log(this.name + age);
+}
+const user = {
+  name: "John",
+  surname: "fearn",
+};
+sayName.call(user, 33);
+sayName.apply(user, [44]);
+
+function count(num) {
+  return this * num;
+}
+
+const double = count.bind(2);
+console.log(double(3));
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  console.log(this);
+});
+
+const obj = {
+  num: 5,
+  sayNumber: function () {
+    const say = () => {
+      console.log(this);
+    };
+    say();
+  },
+};
+
+obj.sayNumber();
