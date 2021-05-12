28bc2cabe4e5948151a3050a00d46ab3fc3ca67e
Demon <Demonid3e@gmail.com>
2021-05-12 14:49:21 +0100

    12.05.21-2

--- New  course/script/script.js
+++ New  course/script/script.js
@@ -1,5 +1,87 @@
 "use strict";
 
+/////// Array and pseudo Array ////////
+
+const arr = [1, 2, 3, 6, 8];
+
+// property length last id of array +1
+console.log(arr.length);
+
+arr.pop(); // removes last element
+
+arr.push(10); // add element to the end
+
+arr.shift(); // deletes first element from array and returns new array
+
+arr.unshift("a"); // adds element to the beggining of array
+
+delete arr[1]; // deletes second element in array
+
+// will create array from string if entered with ","
+
+const str = prompt("", ""); // java,ggg,hhh,jjj
+const products = str.split("", "");
+
+const months = ["Jan", "March", "April", "June"];
+months.splice(1, 0, "Feb");
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, "May");
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
+//
+const animals = ["ant", "bison", "camel", "duck", "elephant"];
+console.log(animals.slice(2));
+// expected output: Array ["camel", "duck", "elephant"]
+
+const months1 = ["March", "Jan", "Feb", "Dec"];
+months1.sort();
+console.log(months1);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();
+console.log(array1);
+// expected output: Array [1, 100000, 21, 30, 4]
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+arr.reverse(); // will reverse the order of elements
+
+arr.concat("item1"); // creates new array in which copies all elements and adds "item1"
+
+const elements = ["Fire", "Air", "Water"];
+
+console.log(elements.join());
+// expected output: "Fire,Air,Water"
+
+console.log(elements.join(""));
+// expected output: "FireAirWater"
+
+console.log(elements.join("-"));
+// expected output: "Fire-Air-Water"
+
+/////////////////////////////////////
+// use of "for" loop in array
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+// use of  "for of" loop // can use break and continue
+for (let value of arr) {
+  console.log(value);
+}
+
+// takes 3 arguments: Item, number in array, and The array forEach() was called upon
+arr.forEach((item, numberInorder, arr) => {
+  console.log(`${item}: ${numberInorder} in array ${arr}`);
+});
+
 // old style of creating
 const abj = new Object();
 
@@ -52,38 +134,38 @@ console.log(border);
 
 ////////Properties and Methods for strings and Numbers///////
 /////////////////////////////////////////////////////////////
+{
+  const str = "test";
+  const arr = [1, 2, 3];
 
-const str = "test";
-const arr = [1, 2, 3];
+  console.log(arr.length); // 3
+  console.log(str.length); // 4
 
-console.log(arr.length); // 3
-console.log(str.length); // 4
+  console.log(str[2]); // s
 
-console.log(str[2]); // s
+  console.log(str.toUpperCase()); // TEST coverts but original kept the same
 
-console.log(str.toUpperCase()); // TEST coverts but original kept the same
+  const fruit = "Some fruit";
 
-const fruit = "Some fruit";
+  console.log(fruit.indexOf("fruit")); // 5
+  console.log(fruit.indexOf("q")); // -1 not found
 
-console.log(fruit.indexOf("fruit")); // 5
-console.log(fruit.indexOf("q")); // -1 not found
+  const logg = "Hello world";
 
-const logg = "Hello world";
+  console.log(logg.slice(6, 11)); // world
+  console.log(logg.slice(6)); // will slice from 6 to the end
+  console.log(logg.slice(-5, -1)); // worl   cuts from the end
 
-console.log(logg.slice(6, 11)); // world
-console.log(logg.slice(6)); // will slice from 6 to the end
-console.log(logg.slice(-5, -1)); // worl   cuts from the end
+  console.log(logg.substring(6, 11)); // same as slice but doesnt support negative
+  console.log(logg.substr(0, 5)); // Hello/ where to start and how many characters
 
-console.log(logg.substring(6, 11)); // same as slice but doesnt support negative
-console.log(logg.substr(0, 5)); // Hello/ where to start and how many characters
+  const round = 12.2;
+  console.log(Math.round(round)); // 12
 
-const round = 12.2;
-console.log(Math.round(round)); // 12
-
-const test = "12.2px";
-console.log(parseInt(test)); // extracts NUMBER 12 from string
-console.log(parseFloat(test)); // extracts FLOAT 12.2 from string
-
+  const test = "12.2px";
+  console.log(parseInt(test)); // extracts NUMBER 12 from string
+  console.log(parseFloat(test)); // extracts FLOAT 12.2 from string
+}
 //////////Functions///////////
 //////////////////////////////
 
--- course/script.js
+++ course/script.js
@@ -1,51 +1,83 @@
 "use strict";
 
-// old style of creating
-const abj = new Object();
+/////// Array and pseudo Array ////////
 
-const options = {
-  name: "test",
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: "black",
-    background: "red",
-  },
-  // creating a new method of an object
-  makeTest: function () {
-    console.log("This is test");
-  },
-};
+const arr = [1, 2, 3, 6, 8];
 
-console.log(options.name);
+// property length last id of array +1
+console.log(arr.length);
 
-// delete options.name;
+arr.pop(); // removes last element
 
-console.log(options);
+arr.push(10); // add element to the end
 
-// cycle trought the object/keys
+arr.shift(); // deletes first element from array and returns new array
 
-let counter = 0;
-for (let key in options) {
-  if (typeof options[key] === "object") {
-    for (let i in options[key]) {
-      console.log(`Property ${i}, has Value: ${options[key][i]} `);
-      counter++;
-    }
-  } else {
-    console.log(`Property ${key}, has Value: ${options[key]} `);
-    counter++;
-  }
+arr.unshift("a"); // adds element to the beggining of array
+
+delete arr[1]; // deletes second element in array
+
+// will create array from string if entered with ","
+
+const str = prompt("", ""); // java,ggg,hhh,jjj
+const products = str.split("", "");
+
+const months = ["Jan", "March", "April", "June"];
+months.splice(1, 0, "Feb");
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, "May");
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
+//
+const animals = ["ant", "bison", "camel", "duck", "elephant"];
+console.log(animals.slice(2));
+// expected output: Array ["camel", "duck", "elephant"]
+
+const months1 = ["March", "Jan", "Feb", "Dec"];
+months1.sort();
+console.log(months1);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();
+console.log(array1);
+// expected output: Array [1, 100000, 21, 30, 4]
+
+function compareNumbers(a, b) {
+  return a - b;
 }
 
-console.log(counter);
+arr.reverse(); // will reverse the order of elements
 
-// iterates trought options.keys and then gets the lenght of the result
-console.log(Object.keys(options).length);
+arr.concat("item1"); // creates new array in which copies all elements and adds "item1"
 
-options.makeTest();
+const elements = ["Fire", "Air", "Water"];
 
-/////Object destructarisation//////
+console.log(elements.join());
+// expected output: "Fire,Air,Water"
 
-const { border, background } = options.colors;
-console.log(border);
+console.log(elements.join(""));
+// expected output: "FireAirWater"
+
+console.log(elements.join("-"));
+// expected output: "Fire-Air-Water"
+
+/////////////////////////////////////
+// use of "for" loop in array
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+// use of  "for of" loop // can use break and continue
+for (let value of arr) {
+  console.log(value);
+}
+
+// takes 3 arguments: Item, number in array, and The array forEach() was called upon
+arr.forEach((item, numberInorder, arr) => {
+  console.log(`${item}: ${numberInorder} in array ${arr}`);
+});
