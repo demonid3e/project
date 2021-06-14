1be6d051497ff44dc238acf4da1e1e10afe30e71
Demon <Demonid3e@gmail.com>
2021-06-14 12:22:59 +0100

    14.06.2021

--- New  course/script/script.js
+++ New  course/script/script.js
@@ -1,8 +1,46 @@
 "use strict";
 
-"use strict";
+//////////////// Select element from DOM \\\\\\\\\\\
+{
+  /* <body>
+    <div class="box" id="box"></div>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <button>5</button>
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="wrapper">...</div>
+    <script src="script.js"></script>
+    <script src="project.js"></script>
+  </body> */
+}
 
-"use strict";
+const box = document.getElementById("box");
+
+console.log(box); //    <div class="box" id="box"></div>
+
+const btns = document.getElementsByTagName("button"); // get pseydoarray class="buttons"
+// doesnt have any methods no properties apart from lenght
+// even if there is only one element it still will be pseydoarray
+const btn2 = document.getElementsByTagName("button")[1]; // will get second button only
+const btn3 = btns[2];
+console.log(btn3); // will get button 3
+
+const circles = document.getElementsByClassName("circle"); // get pseydoarray class="circle"
+
+///////////// Modern style//////////
+
+const hearts = document.querySelectorAll(".hearts"); // using css selectors and has forEach method
+hearts.forEach((item) => {
+  console.log(item);
+});
+
+const oneHeart = document.querySelector(".hearts");
+console.log(oneHeart); // will give first element with css selector "hearts"
+
 ////// Dynamic Data Types ///////
 
 // To String
--- course/script.js
+++ course/script.js
@@ -1,30 +1,40 @@
 "use strict";
-////// Dynamic Data Types ///////
-
-// To String
-console.log(String(null)); // typeof String
-console.log(String(4)); // typeof String
-console.log(5 + ""); // typeof String when adding string always string
-
-const num = 5;
-console.log("https://vk.com/catalog/" + num); // https://vk.com/catalog/5
-
-const fontSize = 26 + "px";
-
-// To Number
-
-console.log(Number("4")); // string to number Typeof number
-console.log(+"5"); // unary + typeof number
-console.log(parseInt("15px", 10)); // 15 typeof number
-
-// let answ = +prompt("Hello", ""); // string to number
-
-// To Boolean
-
-// always false:  0, "", null, undefined, NaN;
-
-let switcher = null;
-
-if (switcher) {
-  console.log("Working...");
+{
+  /* <body>
+    <div class="box" id="box"></div>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <button>5</button>
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="circle"></div>
+    <div class="wrapper">...</div>
+    <script src="script.js"></script>
+    <script src="project.js"></script>
+  </body> */
 }
+
+const box = document.getElementById("box");
+
+console.log(box); //    <div class="box" id="box"></div>
+
+const btns = document.getElementsByTagName("button"); // get pseydoarray class="buttons"
+// doesnt have any methods no properties apart from lenght
+// even if there is only one element it still will be pseydoarray
+const btn2 = document.getElementsByTagName("button")[1]; // will get second button only
+const btn3 = btns[2];
+console.log(btn3); // will get button 3
+
+const circles = document.getElementsByClassName("circle"); // get pseydoarray class="circle"
+
+///////////// Modern style//////////
+
+const hearts = document.querySelectorAll(".hearts"); // using css selectors and has forEach method
+hearts.forEach((item) => {
+  console.log(item);
+});
+
+const oneHeart = document.querySelector(".hearts");
+console.log(oneHeart); // will give first element with css selector "hearts"
